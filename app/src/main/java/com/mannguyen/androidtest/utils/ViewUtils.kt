7ba8567d4bc161adb68f48c698d3bad84fce4402
Man Nguyen <vanman.lqd@gmail.com>
2020-02-12 11:07:49 +0700

    add suppress for deprecation

--- app/src/main/java/com/mannguyen/androidtest/utils/ViewUtils.kt
+++ app/src/main/java/com/mannguyen/androidtest/utils/ViewUtils.kt
@@ -22,6 +22,7 @@ fun AppCompatTextView.setTextFromHtml(html: String?) {
     text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         Html.fromHtml(html ?: "", Html.FROM_HTML_MODE_COMPACT)
     } else {
+        @Suppress("DEPRECATION")
         Html.fromHtml(html ?: "")
     }
 }
